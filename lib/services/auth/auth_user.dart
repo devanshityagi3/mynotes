0168c780a1cab9dd64cfba2bfc7408eba783123d
Devanshi <devanshityagi3@gmail.com>
2022-12-19 21:56:03 +0530

    step-10

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,8 @@ import 'package:flutter/foundation.dart';
 @immutable //koi bhi field changeable na ho
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
   
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
-
+  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified: user.emailVerified);
+ 
 }
